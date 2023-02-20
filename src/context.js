8f8e125846b6ae238304a83970ae5fd279c2d700
Jahid Eljah <jahideljah@gmail.com>
2023-02-20 12:07:32 +0100

    minor update

--- src/context.js
+++ src/context.js
@@ -1,12 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
-import { useCallback } from "react";
+// import { useCallback } from "react";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState("a"); //or e.g "margarita"
+  const [searchTerm, setSearchTerm] = useState("i"); //or e.g "margarita"
   const [cocktails, setCocktails] = useState([]);
 
   const fetchDrinks = async () => {
